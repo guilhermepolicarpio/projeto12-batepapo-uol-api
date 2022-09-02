5f17b1677768369beb98252681b6970a5895b313
Guilherme <guilhermepolicarpio@gmail.com>
2022-09-02 13:49:27 -0300

    Verificação se usuário já está registrado

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import cors from 'cors'
 import {MongoClient} from 'mongodb'
 import dayjs from 'dayjs'
+import Joi from "joi"
 dotenv.config();
 
 const server = express();
@@ -14,9 +15,7 @@ let database;
 const promise = mongoClient.connect();
 
 promise.then(() => {
-  
   database = mongoClient.db("bate_papo_UOL");
-  console.log(database)
 });
 promise.catch(err => console.log(err))
 
@@ -37,23 +36,26 @@ server.post("/participants", async  (req,res) =>{
     }
     
     try{
+        const isRegistred = await database.collection("participants").findOne({name: participant.name})
+        if(isRegistred!==null){
+          res.sendStatus(409);
+          return;
+        }
         const insertedUser =  await database.collection("participants").insertOne(participant);
         const insertedMessage =  await database.collection("messages").insertOne(loginMessage);
-        console.log(insertedUser)
-        console.log(insertedMessage)
-        console.log("oi")
         res.sendStatus(201);
     }catch(error){
-        res.status(500);
-        console.log("errou")
+        res.status(422);
+        console.log("Erro na entrada do participante")
     }
 })
 
 server.get("/participants", async (req,res) =>{
     try{
-      console.log("ola")
+      
       const getUser =  await database.collection("participants").find({}).toArray();
       res.send(getUser);
+      console.log(getUser)
     }catch(error){
       res.send("Erro ao retornar lista de participantes");
   }
@@ -66,11 +68,25 @@ server.get("/messages", async (req,res) =>{
   let messages = await database.collection("messages").find({}).toArray();
   messages.reverse();
   messages.reverse();
-  res.send(messages)
-  console.log(messages)
+  res.send(messages);
 })
-//setInterval(async() => {
-  //console.log("ola")
-//}, 1500);
+
+setInterval(async() => {
+  const getUsers =  await database.collection("participants").find({}).toArray();
+
+  getUsers.map(async (user) =>{
+    if(Date.now() - user.lastStatus > 10000){
+      const exitMessage ={
+      from: user.name,
+      to: "Todos",
+      text: "sai da sala...",
+      type: "status",
+      time: `${dayjs().hour()}:${dayjs().minute()}:${dayjs().second()}`
+      }
+      await database.collection("participants").deleteOne({name:user.name});
+      await database.collection("messages").insertOne(exitMessage)
+    }
+  })
+}, 15000);
 
 server.listen(5000, () => console.log("Rodando"))
