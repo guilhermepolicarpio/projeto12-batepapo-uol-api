a00e872a0f211eb7b67ed7780e67d04c1355a2b0
Guilherme <guilhermepolicarpio@gmail.com>
2022-09-05 09:52:35 -0300

    Atualização do status, ajustes nas mensagens de erro e alteração da função lastStatus

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Router } from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors'
 import {MongoClient} from 'mongodb'
@@ -32,10 +32,16 @@ server.post("/participants", async  (req,res) =>{
       to: "Todos",
       text: "entra na sala...",
       type: "status",
-      time: `${dayjs().hour()}:${dayjs().minute()}:${dayjs().second()}`
+      time: dayjs().format('HH:mm:ss')
     }
-    
-    try{
+
+    const schema = Joi.object({
+      name: Joi.string().required()
+    })
+
+   const { error } = schema.validate({ name: body.name});
+   
+   if(!error){
         const isRegistred = await database.collection("participants").findOne({name: participant.name})
         if(isRegistred!==null){
           res.sendStatus(409);
@@ -44,32 +50,93 @@ server.post("/participants", async  (req,res) =>{
         const insertedUser =  await database.collection("participants").insertOne(participant);
         const insertedMessage =  await database.collection("messages").insertOne(loginMessage);
         res.sendStatus(201);
-    }catch(error){
-        res.status(422);
-        console.log("Erro na entrada do participante")
     }
+  return res.status(422);
 })
 
 server.get("/participants", async (req,res) =>{
     try{
-      
       const getUser =  await database.collection("participants").find({}).toArray();
       res.send(getUser);
-      console.log(getUser)
     }catch(error){
       res.send("Erro ao retornar lista de participantes");
   }
 });
 
+server.post("/messages", async(req,res) => {
+ 
+  const {to, text, type} = req.body;
+  const from = req.headers.user;
+  const time = dayjs().format('HH:mm:ss')
+  const user = await database.collection("participants").findOne({name: from})
+
+  const message={
+    from: from,
+    to: to,
+    text: text,
+    type: type,
+    time: time
+  }
+
+  const messageSchema = Joi.object({
+      from: Joi.string().required(),
+      to: Joi.string().required(),
+      text: Joi.string().required(),
+      type: Joi.string().required(),
+      time: Joi.string().required()
+  });
+
+  const validation = messageSchema.validate(message)
+
+  if(validation.error){
+    console.log("Erro de validação");
+    res.status(422).send("Erro de validação");
+    return;
+  }
+ 
+try{
+  if(!user){
+    res.status(422).send("Participante não cadastrado");
+    console.log("Participante não cadastradoo");
+    return;
+  }
+  await database.collection("messages").insertOne(message)
+  return res.sendStatus(201);
+}catch{ (err)
+  return res.sendStatus(422);
+}
+});
+
 server.get("/messages", async (req,res) =>{
   const limit = req.query.limit;
   const user = req.headers.user;
-
   let messages = await database.collection("messages").find({}).toArray();
+
+  messages = messages.filter(available => available.type === "message" || available.type==="status" || available.to === "user" || available.from==="user")
+  messages = messages.slice(-limit);
   messages.reverse();
   messages.reverse();
   res.send(messages);
 })
+
+server.post("/status", async (req,res) => {
+  const user = req.headers.user;
+  try{
+    const checkUser = await database.collection("participants").findOne({name: user})
+    if(!checkUser){
+      res.status(404).send("Participante não cadastrado");
+      console.log("Participante não cadastradoo");
+      return;
+    }
+    await database.collection("participants").updateOne(
+      {name: user},
+      {$set: {lastStatus: Date.now()}}
+    );
+    res.sendStatus(200);
+  }catch{
+    res.send("Erro ao buscar participante");
+  }
+});
 
 setInterval(async() => {
   const getUsers =  await database.collection("participants").find({}).toArray();
@@ -81,7 +148,7 @@ setInterval(async() => {
       to: "Todos",
       text: "sai da sala...",
       type: "status",
-      time: `${dayjs().hour()}:${dayjs().minute()}:${dayjs().second()}`
+      time: dayjs().format('HH:mm:ss')
       }
       await database.collection("participants").deleteOne({name:user.name});
       await database.collection("messages").insertOne(exitMessage)
